0398d51dcb96bd88564d2fc3005484f6c040323f
lou <dustyfingers01257@gmail.com>
2021-11-07 15:26:40 -0700

    subscribe to topic for each chat room, made messages display once sent to chat

--- chat/lib/chat_web/live/room_live.ex
+++ chat/lib/chat_web/live/room_live.ex
@@ -6,12 +6,24 @@ defmodule ChatWeb.RoomLive do
   @impl true
   # grab id from url params
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+    ChatWeb.Endpoint.subscribe(topic)
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: [])}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(message: message)
+    # broadcasts the message to the clients subscribed to the topic
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, socket}
+  end
+
+  # handle_info functions handle broadcasts by the server
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    Logger.info(payload: message)
+    # [socket.assigns.messages | message] appends new message onto existing list
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
   end
 end
